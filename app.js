d4edf93e2dc560101d629f019806f6de646e0db5
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-27 21:00:53 +0500

    Update app.js

--- app.js
+++ app.js
@@ -25,15 +25,15 @@ function getuser() {
 
 btn.addEventListener("click", getuser); 
 function showdata(main) {
-    githubProfiler.innerHTML = ` <img src="${main.avatar_url}"  alt="...">`
-    // githubProfiler.innerHTML = `
-    // < div class="card" style = "width: 18rem;" >
-    //     <img src="..." class="card-img-top" alt="...">
-    //         <div class="card-body">
-    //             <h5 class="card-title">Card title</h5>
-    //             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    //             <a href="#" class="btn btn-primary">Go somewhere</a>
-    //         </div>
-    //     </div>`
+    
+    githubProfiler.innerHTML = `
+    < div class="card" style = "width: 18rem;" >
+        <img src="${main.avatar_url}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>`
 }
 
