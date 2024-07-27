c6ca2dd9b0001773d846bf6bdbc05dc7e253c538
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-28 01:03:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ function showdata(main) {
         <img src="${main.avatar_url}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${main.bio}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 class="card-title">${main.name}</h5>
+                <p class="card-text"> .</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>`
