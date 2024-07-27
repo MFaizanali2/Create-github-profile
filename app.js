49094f1d618cd3a9c0fa485d33d5e1fe1bfbd297
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-27 20:45:10 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,33 +8,32 @@ function getuser() {
     event.preventDefault();
 
     fetch(`https://api.github.com/users/${github.value}`)
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data)
+        showdata(data);
+        
+    })
+    .catch((error) => {
+        console.log(error)
+    })
     console.log(github.value)
-
-        .then((res) => {
-            return res.json();
-        })
-        .then((data) => {
-            console.log(data)
-            showdata("allow");
-
-        })
-        .catch((error) => {
-            console.log(error)
-        })
 }
 
 
-btn.addEventListener("click", getuser);
+btn.addEventListener("click", getuser); 
 function showdata(main) {
     githubProfiler.innerHTML = ` <img src="${main.avatar_url}"  alt="...">`
-    githubProfiler.innerHTML = `
-    < div class="card" style = "width: 18rem;" >
-        <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>`
+    // githubProfiler.innerHTML = `
+    // < div class="card" style = "width: 18rem;" >
+    //     <img src="..." class="card-img-top" alt="...">
+    //         <div class="card-body">
+    //             <h5 class="card-title">Card title</h5>
+    //             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    //             <a href="#" class="btn btn-primary">Go somewhere</a>
+    //         </div>
+    //     </div>`
 }
 
